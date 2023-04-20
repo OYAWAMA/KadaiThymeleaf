f1408ff0d1dbdf9fad13d68af8f008091781ff85
OYAWAMA <fluting.dalton_0m@icloud.com>
2023-04-20 21:27:21 +0900

    input修正

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -8,18 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class FormController {
-
-    @GetMapping( value= {"/input", "/input/{previous}"})
-    public String getForm(@PathVariable(value = "previous", required = false) String previous, Model model) {
-        // クエリパラメータから受け取った値をModelに登録
-        if (previous != null) {
-            model.addAttribute("previous", previous);
-        }
-        // input.htmlに画面遷移
-        return "input";
-    }
-
+public class OutputController {
 
     @PostMapping("/output")
     public String postForm(@RequestParam("val") String val, Model model) {
